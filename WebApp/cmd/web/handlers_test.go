4b7180c22bb6480b0910ab4270aa88f28e47a7e4
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-12-28 11:40:39 +0200

    45. Updating our tests

--- WebApp/cmd/web/handlers_test.go
+++ WebApp/cmd/web/handlers_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -34,4 +37,34 @@ func Test_application_handlers(t *testing.T) {
 			t.Errorf("for \"%s\": expected status is %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 		}
 	}
+}
+
+func TestAppHome(t *testing.T) {
+	//	create a request
+	req, _ := http.NewRequest("GET", "/", nil)
+	req = addContextAndSessionToRequest(req, app)
+	//	create a response writer
+	rr := httptest.NewRecorder()
+	//	create a handler
+	handler := http.HandlerFunc(app.Home)
+	handler.ServeHTTP(rr, req)
+	//	check status code
+	if rr.Code != http.StatusOK {
+		t.Errorf("TestAppHome returned wrong status code; expected %d, but got %d", http.StatusOK, rr.Code)
+	}
+	body, _ := io.ReadAll(rr.Body)
+	if !strings.Contains(string(body), "<small>From session:") {
+		t.Error("Home page rendering error")
+	}
+}
+
+func getCtx(r *http.Request) context.Context {
+	ctx := context.WithValue(r.Context(), userIpKey, "unknown")
+	return ctx
+}
+
+func addContextAndSessionToRequest(r *http.Request, app application) *http.Request {
+	r = r.WithContext(getCtx(r))
+	ctx, _ := app.Session.Load(r.Context(), r.Header.Get("X-Session"))
+	return r.WithContext(ctx)
 }
