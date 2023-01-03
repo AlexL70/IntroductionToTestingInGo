704783d50d2c0cd6728c27195bf6eaaedde5f606
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-03 17:09:45 +0200

    67. Updating setup_test.go to use the test repository

--- WebApp/cmd/web/setup_test.go
+++ WebApp/cmd/web/setup_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"testing"
 
@@ -13,14 +12,8 @@ var app application
 func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 	app.Session = getSession()
-	app.DSN = "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
 
-	conn, err := app.connectToDb()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-	app.DB = &dbrepo.PostgresDbRepo{DB: conn}
+	app.DB = &dbrepo.TestDbRepo{}
 
 	os.Exit(m.Run())
 }
