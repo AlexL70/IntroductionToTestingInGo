8157563621ccef25b5b5e520d92682a9eb68b313
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-04 22:12:37 +0200

    83. Adding the UserImage type to the User type

--- WebApp/pkg/repository/dbrepo/users_postgres.go
+++ WebApp/pkg/repository/dbrepo/users_postgres.go
@@ -66,11 +66,13 @@ func (m *PostgresDbRepo) GetUser(id int) (*data.User, error) {
 
 	query := `
 		select 
-			id, email, first_name, last_name, password, is_admin, created_at, updated_at 
+			u.id, u.email, u.first_name, u.last_name, u.password, u.is_admin, u.created_at, u.updated_at,
+			coalesce(ui.file_name, '')
 		from 
-			users 
+			users u
+			left join user_images ui on (ui.user_id = u.id)
 		where 
-		    id = $1`
+		    u.id = $1`
 
 	var user data.User
 	row := m.DB.QueryRowContext(ctx, query, id)
@@ -84,8 +86,8 @@ func (m *PostgresDbRepo) GetUser(id int) (*data.User, error) {
 		&user.IsAdmin,
 		&user.CreatedAt,
 		&user.UpdatedAt,
+		&user.ProfilePic.FileName,
 	)
-
 	if err != nil {
 		return nil, err
 	}
@@ -100,11 +102,13 @@ func (m *PostgresDbRepo) GetUserByEmail(email string) (*data.User, error) {
 
 	query := `
 		select 
-			id, email, first_name, last_name, password, is_admin, created_at, updated_at 
+			u.id, u.email, u.first_name, u.last_name, u.password, u.is_admin, u.created_at, u.updated_at,
+			coalesce(ui.file_name, '')
 		from 
-			users 
+			users u
+			left join user_images ui on (ui.user_id = u.id)
 		where 
-		    email = $1`
+		    u.email = $1`
 
 	var user data.User
 	row := m.DB.QueryRowContext(ctx, query, email)
@@ -118,6 +122,7 @@ func (m *PostgresDbRepo) GetUserByEmail(email string) (*data.User, error) {
 		&user.IsAdmin,
 		&user.CreatedAt,
 		&user.UpdatedAt,
+		&user.ProfilePic.FileName,
 	)
 
 	if err != nil {
