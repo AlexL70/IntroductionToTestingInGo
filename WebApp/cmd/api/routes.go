bf363f4b9d7008039844a326cf688b78fe3f0c62
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-10 16:28:43 +0200

    92. Adding stub handlers (endpoints) for our API

--- WebApp/cmd/api/routes.go
+++ WebApp/cmd/api/routes.go
@@ -14,10 +14,21 @@ func (app *application) routes() http.Handler {
 	//	mux.Use(app.enableCORS)
 
 	//	authentication routes: auth handler and refresh handler
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
 
 	//	test handler (to be able to ping API)
 
 	//	protected routes
+	mux.Route("/users", func(mux chi.Router) {
+		//	use auth middleware
+
+		mux.Get("/", app.AllUsers)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/", app.insertUser)
+		mux.Patch("/{userID}", app.updateUser)
+	})
 
 	return mux
 }
