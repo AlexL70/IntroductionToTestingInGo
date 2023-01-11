b15217be83e0b6955361633e8a5a237a047a16a7
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-11 18:29:19 +0200

    96. Implementing the authenticate handler

--- WebApp/cmd/api/api-handlers.go
+++ WebApp/cmd/api/api-handlers.go
@@ -1,17 +1,45 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+)
+
+type Credentials struct {
+	UserName string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
 	//	read a json payload
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	//	look up the user by email address
+	user, err := app.DB.GetUserByEmail(creds.UserName)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	//	check password
+	ok, err := user.PasswordMatches(creds.Password)
+	if !ok || err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+	}
 
 	//	generate tokens (if password matches)
+	pair, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+	}
 
 	// 	send token to user
+	_ = app.writeJSON(w, http.StatusOK, pair)
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
