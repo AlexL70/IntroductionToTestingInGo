a03f67383d51b3c30a7ccc94a528471b60b60267
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-01-13 11:09:05 +0200

    104. Add middleware to routes

--- WebApp/cmd/api/api-routes.go
+++ WebApp/cmd/api/api-routes.go
@@ -11,26 +11,15 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	//	register middleware
 	mux.Use(middleware.Recoverer)
-	//	mux.Use(app.enableCORS)
+	mux.Use(app.enableCORS)
 
 	//	authentication routes: auth handler and refresh handler
 	mux.Post("/auth", app.authenticate)
 	mux.Post("/refresh-token", app.refresh)
 
-	//	test handler (to be able to ping API)
-	mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		var payload = struct {
-			Message string `json:"message"`
-		}{
-			Message: "Hello World!",
-		}
-
-		_ = app.writeJSON(w, http.StatusOK, payload)
-	})
-
 	//	protected routes
 	mux.Route("/users", func(mux chi.Router) {
-		//	use auth middleware
+		mux.Use(app.authRequired)
 
 		mux.Get("/", app.AllUsers)
 		mux.Get("/{userID}", app.getUser)
